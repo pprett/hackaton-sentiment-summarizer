7d2fae8cace5192ab344612752be2de744dc9266
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2011-11-01 11:21:00 +0100

    first commit

---
+++ src/contentScript.js
@@ -0,0 +1,93 @@
+/**
+ * Contentscript
+ *
+ * Use jQuery to select all review micro formats from the DOM.
+ *
+ */
+
+console.log("PageLoaded");
+console.log(window.location.href);
+
+function scrape() {
+    console.log("scraping...");
+    var review_divs = [];
+    if (window.location.href.match(/^http:\/\/twitter\.com/)) {
+        console.log('looking for tweets twitter');
+        review_divs = document.querySelectorAll('div.tweet-row > div.tweet-text');
+
+    } else {
+        console.log('looking for micro-formats');
+        review_divs = document.querySelectorAll(
+	    '*[itemprop="reviews"] *[itemprop="reviewBody"]');
+    }
+
+    var num_chars = 0;
+
+    var reviews = [];
+    for (var i = 0; i < review_divs.length; i++) {
+        var review_text = review_divs[i].textContent;
+        var review = {content: review_text, lang:'en'};
+        reviews.push(review);
+        num_chars += review_text.length;
+	// send 10K chars at most.
+        if (num_chars >= 10000)
+	    break;
+    }
+
+    if (reviews.length > 0) {
+        console.log(reviews.length + " reviews found");
+    } else {
+        console.log("No Reviews Found");
+    }
+
+    if (reviews.length > 0) {
+
+        var data = JSON.stringify({data: reviews});
+        window.$.post('http://sentimentanalyzer.appspot.com/api/classify.json',
+		 data,
+		 function(res) {
+		     console.log("Got results");
+		     if ('data' in res) {
+			 var reviews = res["data"];
+			 var scores = [];
+			 var num_pos = 0;
+  		         for (i = 0; i < reviews.length; i++) {
+			     var score = parseFloat(reviews[i].score);
+			     var pred = 0;
+			     if (score >= 0.5) {
+				 pred = 1;
+			     }
+			     if (pred == 1) {
+				 num_pos += 1;
+			     }
+			     scores.push(pred);
+			 }
+			 //console.log(scores);
+			 console.log(num_pos + " positive reviews; ");
+			 console.log((scores.length - num_pos) + " negative reviews; ");
+			 chrome.extension.sendRequest({action:'showPageAction',
+						       num_pos: num_pos,
+						       num_reviews: scores.length},
+						      function(response) {});
+		     } else {
+			 // got error
+			 console.log("Error: " + res);
+		     }
+		 }, 'json');
+
+    }
+
+}
+
+/**
+ * Check if we're on twitter - if so wait for 5sec until tweets
+ * are loaded and then run scraper, otherwise run scraper
+ * immediately
+ */
+if (window.location.href.match(/^http:\/\/twitter\.com/)) {
+    console.log('on twitter - lets wait some msec and then scrape');
+    window.setTimeout(scrape, 5000);
+} else {
+    console.log('call scraper');
+    scrape();
+}
---
+++ src/popup.js
@@ -0,0 +1,21 @@
+/**
+ * On load of popup.html: access background page and retrieve
+ * the stats. Display stats using Google image API.
+ */
+function loader(){
+  chrome.tabs.getSelected(null, function(tab) {
+    var tab_id = tab.id;
+    var bg = chrome.extension.getBackgroundPage();
+    var num_reviews = bg.num_reviews[tab_id];
+    var num_pos = bg.num_pos[tab_id];
+    bg.console.log("Draw chart with: " + num_pos + " - " + num_reviews);
+    var num_reviews_str = num_reviews + ',' + num_reviews;
+    var pos_neg_str = num_pos + ',' + (num_reviews - num_pos);
+    window.$("div#chart_div").html('<img src="https://chart.googleapis.com/chart?cht=p3&chs=250x100&chco=00EE00,FF0000&chd=t:' + num_pos + ',' + (num_reviews - num_pos) + '&chl=positive|negative" />');
+    window.$("div#stats_div").html('<span>' + num_reviews + ' reviews extracted.</span>');
+
+  });
+
+}
+
+window.onload = loader;
